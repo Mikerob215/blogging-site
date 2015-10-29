09d7efd0ce2523db57d4e227358f61e8f920887b
mike <mikerobinson1@gmail.com>
2015-10-29 12:58:37 -0400

    ended up not styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :users do
-    resources :posts
-    resources :comments
-    resources :replies
+  resources :users, shallow: true do
+    resources :posts do
+      resources :comments do
+        resources :replies
+      end
+    end
+
+
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
