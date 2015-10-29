a5a53c02182e782a4fbd6cb81d3b6a0f7819c9bd
mike <mikerobinson1@gmail.com>
2015-10-29 16:27:08 -0400

    too much stuff, switching to posts

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '4.2.4'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
-#gem 'sass-rails', '~> 5.0'
+gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
@@ -28,7 +28,7 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use bootswatch
 gem 'bootstrap-sass', '~> 3.3.5'
-gem 'sass-rails', '>= 3.2'
+#gem 'sass-rails', '>= 3.2'
 gem 'bootswatch-rails', '~> 3.3', '>= 3.3.5'
 
 # Use Unicorn as the app server
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,32 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
   end
 
   def show
-    @users = User.all
+    @user = User.find(params[:id])
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def new
+    @user = User.new
   end
-  
+
   def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
