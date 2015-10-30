352a1796df79f32db5fddd835b9681b81d14d049
mike <mikerobinson1@gmail.com>
2015-10-30 16:56:41 -0400

    functional login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+  def new
+end
+
+def create
+  user = User.find_by(email: params[:session][:email].downcase)
+   if user && user.authenticate(params[:session][:password])
+    session[:user_id] = user.id
+     redirect_to user
+    else
+      redirect_to '/login'
+  end
+end
+
+def destroy
+   session[:user_id] = nil
+   redirect_to '/login'
+ end
+
+ private
+ def user_params
+   params.require(:user).permit(:name, :email, :password,
+                                :password_confirmation)
+ end
+
 end
